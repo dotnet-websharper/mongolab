31c8ae3507a76b19c8603d42f1c387405dcd8918
Loic Denuziere <loic.denuziere@intellifactory.com>
2015-10-06 12:25:18 +0200

    Use F# 3.0

--- build.fsx
+++ build.fsx
@@ -4,15 +4,12 @@ open IntelliFactory.Build
 let bt =
     BuildTool().PackageId("WebSharper.MongoLab")
         .VersionFrom("WebSharper")
+        .WithFSharpVersion(FSharpVersion.FSharp30)
         .WithFramework(fun fw -> fw.Net40)
 
 let main =
     bt.WebSharper.Library("WebSharper.MongoLab")
         .SourcesFromProject()
-        .References(fun rb ->
-            [
-                rb.NuGet("WebSharper").Reference ()
-            ])
 
 bt.Solution [
 
